7d84a1f1c3d164e26661ef5aa2f28efacada5851
hanh361 <hanhltmt3@gmail.com>
2022-05-18 20:06:39 +0700

    clone

--- src/pages/login/LoginPage.js
+++ src/pages/login/LoginPage.js
@@ -1,89 +1,68 @@
-import React,{useState} from 'react';
-import { useForm } from "react-hook-form";
-import ReactLoading from 'react-loading';
-import { Link, useNavigate } from 'react-router-dom';
+import React, { useState } from "react";
+import ReactLoading from "react-loading";
+import { Link, useNavigate } from "react-router-dom";
+import LoginFrom from "./../../features/login/FromLogin";
+import serviceCallApi from "./../../services/serviceApi";
 
 const LoginPage = () => {
-    const [loading, setLoading] = useState(false);
-    const navigate = useNavigate();
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
 
-    const { register, handleSubmit, watch, formState: { errors } } = useForm();
-    const onSubmit = (data) => {
-        setLoading(true);
+  const onSubmit = async (data) => {
+    setLoading(true);
+    setLoading(true);
+    try {
+      const result = await serviceCallApi("login", "POST", data);
 
-        setTimeout(() => {
-            setLoading(false);
-            navigate('/');
-        }, 5000);
+      setLoading(false);
+      localStorage.setItem("userInfo", JSON.stringify(result.data.data));
+      navigate("/");
+    } catch (error) {
+      setLoading(false);
+      alert(error.message);
     }
-    return (
-        <section className="h-100">
-        <div className="container h-100">
-          <div className="row justify-content-sm-center h-100">
-            <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-              <div className="text-center my-5">
-               <h1>Login</h1>
-              </div>
-              <div className="card shadow-lg">
-                <div className="card-body p-5">
-                 
-                  {loading ? (
-                      <div className="d-flex justify-content-center">
-                          <ReactLoading 
-                      type='spin' 
-                      color='blue' 
-                      height={'20%'} 
-                      width={'20%'} 
-                      />
-                      </div>
-                  ): (<form onSubmit={handleSubmit(onSubmit)} className="needs-validation" noValidate autoComplete="off">
-                  <div className="mb-3">
-                    <label className="mb-2 text-muted" htmlFor="email">E-Mail Address</label>
-                    <input id="email" type="email" className="form-control" name="email"  {...register("email", { required: true })}/>
-                    <div className="text-danger">
-                    {errors.email && <span>This field is required</span>}
-                    </div>
-                  </div>
-                  <div className="mb-3">
-                    <div className="mb-2 w-100">
-                      <label className="text-muted" htmlFor="password">Password</label>
-                      <a href="forgot.html" className="float-end">
-                        Forgot Password?
-                      </a>
-                    </div>
-                    <input id="password" type="password" className="form-control" name="password" {...register("password", { required: true })} />
-                    <div className="text-danger">
-                    {errors.password && <span>This field is required</span>}
-                    </div>
-                  </div>
-                  <div className="d-flex align-items-center">
-                    <div className="form-check">
-                      <input type="checkbox" name="remember" id="remember" className="form-check-input" />
-                      <label htmlFor="remember" className="form-check-label">Remember Me</label>
-                    </div>
-                    <button type="submit" className="btn btn-primary ms-auto">
-                      Login
-                    </button>
-                  </div>
-                </form>)}
-                  
+  };
 
-                 
-                </div>
-                <div className="card-footer py-3 border-0">
-                  <div className="text-center">
-                    Don't have an account? <Link to="/register" className="text-dark">Create One</Link>
+  return (
+    <section className="h-100">
+      <div className="container h-100">
+        <div className="row justify-content-sm-center h-100">
+          <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+            <div className="text-center my-5">
+              <h1>Login </h1>
+            </div>
+            <div className="card shadow-lg">
+              <div className="card-body p-5">
+                {loading ? (
+                  <div className="text-center d-flex justify-content-center">
+                    <ReactLoading
+                      type="spin"
+                      color="blue"
+                      height={"20%"}
+                      width={"20%"}
+                    />
                   </div>
+                ) : (
+                  <LoginFrom onSubmit={onSubmit} />
+                )}
+              </div>
+              <div className="card-footer py-3 border-0">
+                <div className="text-center">
+                  Already have an account?{" "}
+                  <Link to="/register" className="text-dark">
+                    Register
+                  </Link>
                 </div>
               </div>
-              <div className="text-center mt-5 text-muted">
-                Copyright © 2017-2021 — Your Company 
-              </div>
+            </div>
+            <div className="text-center mt-5 text-muted">
+              Copyright © Your Company
             </div>
           </div>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 };
 
 export default LoginPage ;
--- src/pages/register/RegisterPage.js
+++ src/pages/register/RegisterPage.js
@@ -1,37 +1,120 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-const Register = () => {
-    return (
-        <section className="h-100">
-        <div className="container h-100">
-          <div className="row justify-content-sm-center h-100">
-            <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
-              <div className="text-center my-5">
-                <img src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg" alt="logo" width={100} />
-              </div>
-              <div className="card shadow-lg">
-                <div className="card-body p-5">
-                  <h1 className="fs-4 card-title fw-bold mb-4">Register</h1>
-                  <form method="POST" className="needs-validation" noValidate autoComplete="off">
+import React, { useState } from "react";
+import ReactLoading from "react-loading";
+import { Link, useNavigate } from "react-router-dom";
+import serviceCallApi from "../../services/serviceApi";
+import { useForm } from "react-hook-form";
+
+const RegisterPage = () => {
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const {
+    register,
+    handleSubmit,
+    watch,
+    formState: { errors },
+  } = useForm();
+  const onSubmit = async (data) => {
+    setLoading(true);
+    try {
+      await serviceCallApi("register", "POST", data);
+      setLoading(false);
+      navigate("/login");
+    } catch (error) {
+      setLoading(false);
+      console.log(error);
+    }
+  };
+  return (
+    <section className="h-100">
+      <div className="container h-100">
+        <div className="row justify-content-sm-center h-100">
+          <div className="col-xxl-4 col-xl-5 col-lg-5 col-md-7 col-sm-9">
+            <div className="text-center my-5">
+              <img
+                src="https://getbootstrap.com/docs/5.0/assets/brand/bootstrap-logo.svg"
+                alt="logo"
+                width={100}
+              />
+            </div>
+            <div className="card shadow-lg">
+              <div className="card-body p-5">
+                <h1 className="fs-4 card-title fw-bold mb-4">Register</h1>
+                {loading ? (
+                  <div className="text-center d-flex justify-content-center">
+                    <ReactLoading
+                      type="spin"
+                      color="blue"
+                      height={"20%"}
+                      width={"20%"}
+                    />
+                  </div>
+                ) : (
+                  <form
+                    onSubmit={handleSubmit(onSubmit)}
+                    className="needs-validation"
+                  >
                     <div className="mb-3">
-                      <label className="mb-2 text-muted" htmlFor="name">Name</label>
-                      <input id="name" type="text" className="form-control" name="name" defaultValue required />
-                      <div className="invalid-feedback">
-                        Name is required	
+                      <label className="mb-2 text-muted" htmlFor="name">
+                        Name
+                      </label>
+                      <input
+                        id="name"
+                        type="text"
+                        className="form-control"
+                        {...register("name", { required: true })}
+                        required
+                      />
+                      <div className="text-danger">
+                        {errors.name && <span>Name field is required</span>}
                       </div>
                     </div>
                     <div className="mb-3">
-                      <label className="mb-2 text-muted" htmlFor="email">E-Mail Address</label>
-                      <input id="email" type="email" className="form-control" name="email" defaultValue required />
-                      <div className="invalid-feedback">
-                        Email is invalid
+                      <label className="mb-2 text-muted" htmlFor="email">
+                        E-Mail Address
+                      </label>
+                      <input
+                        id="email"
+                        type="email"
+                        className="form-control"
+                        {...register("email", { required: true })}
+                        required
+                      />
+                      <div className="text-danger">
+                        {errors.name && <span>Email field is required</span>}
                       </div>
                     </div>
                     <div className="mb-3">
-                      <label className="mb-2 text-muted" htmlFor="password">Password</label>
-                      <input id="password" type="password" className="form-control" name="password" required />
-                      <div className="invalid-feedback">
-                        Password is required
+                      <label className="mb-2 text-muted" htmlFor="password">
+                        Password
+                      </label>
+                      <input
+                        id="password"
+                        type="password"
+                        className="form-control"
+                        {...register("password", { required: true })}
+                        required
+                      />
+                      <div className="text-danger">
+                        {errors.password && (
+                          <span>Password field is required</span>
+                        )}
+                      </div>
+                    </div>
+                    <div className="mb-3">
+                      <label className="mb-2 text-muted" htmlFor="password">
+                        Confirm Password
+                      </label>
+                      <input
+                        id="c_password"
+                        type="password"
+                        className="form-control"
+                        {...register("c_password", { required: true })}
+                        required
+                      />
+                      <div className="text-danger">
+                        {errors.c_password && (
+                          <span>Confirm password field is required</span>
+                        )}
                       </div>
                     </div>
                     <p className="form-text text-muted mb-3">
@@ -39,25 +122,29 @@ const Register = () => {
                     </p>
                     <div className="align-items-center d-flex">
                       <button type="submit" className="btn btn-primary ms-auto">
-                        Register	
+                        Register
                       </button>
                     </div>
                   </form>
-                </div>
-                <div className="card-footer py-3 border-0">
-                  <div className="text-center">
-                    Already have an account? <Link to='/login' className="text-dark">Login</Link>
-                  </div>
+                )}
+              </div>
+              <div className="card-footer py-3 border-0">
+                <div className="text-center">
+                  Already have an account?{" "}
+                  <Link to="/login" className="text-dark">
+                    Login
+                  </Link>
                 </div>
               </div>
-              <div className="text-center mt-5 text-muted">
-                Copyright © 2017-2021 — Your Company 
-              </div>
+            </div>
+            <div className="text-center mt-5 text-muted">
+              Copyright © 2017-2021 — Your Company
             </div>
           </div>
         </div>
-      </section>
-    );
+      </div>
+    </section>
+  );
 };
 
-export default Register;
+export default RegisterPage;
